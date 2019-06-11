b3573a3a155c2596f1437876c06904f403175b4e
jeremy1111 <jk.hui@hotmail.com>
2019-06-11 10:26:37 +0100

    Update PianoCompetition.java

--- PianoCompetition.java
+++ PianoCompetition.java
@@ -7,8 +7,8 @@ public class PianoCompetition
    /**Constructor */
    public PianoCompetition(JudgePanel aJudgePanel, String aMusicPiece)
    {
-   this.judgePanel = aJudgePanel;
-   this.musicPiece = aMusicPiece;
+      this.judgePanel = aJudgePanel;
+      this.musicPiece = aMusicPiece;
    }
    /**Methods*/
    /** Sets the judges' names by invoking component's method
@@ -17,28 +17,28 @@ public class PianoCompetition
     */
    public void setJudgeNames(String someNames)
    {
-   if (this.judgePanel == null)
-   {
-      this.judgePanel = new JudgePanel(someNames, null, false);
-   }
-   else
-   {
-      this.judgePanel.setNames(someNames);
-   }
+      if (this.judgePanel == null)
+      {
+         this.judgePanel = new JudgePanel(someNames, null, false);
+      }
+      else
+      {
+         this.judgePanel.setNames(someNames);
+      }
    }
    /** Getter for judge's names if judgePanel object exists. 
     * If it doesn't, it returns the string 'Unknown' 
     */
    public String getJudgeNames()
    {
-   if (this.judgePanel == null || this.judgePanel.getCountries() == null)
-   {
-      return "Unknown";
-   }
-   else
-   {
-      return this.judgePanel.getNames();
-   }
+      if (this.judgePanel == null || this.judgePanel.getCountries() == null)
+      {
+         return "Unknown";
+      }
+      else
+      {
+         return this.judgePanel.getNames();
+      }
    }
    /** Sets the judges' countries by invoking component's method
     * if judgePanel object exists. If it doesn't, it 
@@ -46,28 +46,28 @@ public class PianoCompetition
     */
    public void setJudgeCountries(String someCountries)
    {
-   if (this.judgePanel == null)
-   {
-      this.judgePanel = new JudgePanel(null, someCountries, false);
-   }
-   else
-   {
-      this.judgePanel.setCountries(someCountries);
-   }
+      if (this.judgePanel == null)
+      {
+         this.judgePanel = new JudgePanel(null, someCountries, false);
+      }
+      else
+      {
+         this.judgePanel.setCountries(someCountries);
+      }
    }
    /** Getter for judge's countries if judgePanel object exists. 
     * If it doesn't, it returns the string 'Unknown' 
     */
    public String getJudgeCountries()
    {
-   if (this.judgePanel == null || this.judgePanel.getCountries() == null)
-   {
-      return "Unknown";
-   }
-   else
-   {
-      return this.judgePanel.getCountries();
-   }
+      if (this.judgePanel == null || this.judgePanel.getCountries() == null)
+      {
+         return "Unknown";
+      }
+      else
+      {
+         return this.judgePanel.getCountries();
+      }
    }
    /** Sets the name of the music piece */
    public void setMusicPiece(String aMusicPiece)
@@ -85,22 +85,22 @@ public class PianoCompetition
     */
    public String findOutVerdict()
    {
-   String panelReaction;
-   if (this.judgePanel.getVerdict() == true)
-   {
-      panelReaction = "they ask for encore";  
-   }
-   else
-   {
-      panelReaction = "they eliminate the candidate";
-   }
-   return panelReaction;
+      String panelReaction;
+      if (this.judgePanel.getVerdict() == true)
+      {
+         panelReaction = "they ask for encore";  
+      }
+      else
+      {
+         panelReaction = "they eliminate the candidate";
+      }
+      return panelReaction;
    }
    /** Returns string describing object, content will depend
     * on judge panel's decision */
    public String toString()
    {
-   return "The recital of " + this.musicPiece + " is judged by " +
-   this.judgePanel.toString() + " and " + this.findOutVerdict();
+      return "The recital of " + this.musicPiece + " is judged by " +
+      this.judgePanel.toString() + " and " + this.findOutVerdict();
    }
 }
